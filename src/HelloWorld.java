425760f55763f18a2a939bd876a54c489837a2e1
senay12 <46760944+senay12@users.noreply.github.com>
2019-01-19 12:03:39 -0600

    add one more step

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("good Evening");
+        System.out println("thank you");
     }
+    
 }
